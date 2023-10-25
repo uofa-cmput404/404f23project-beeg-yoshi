32cf393d93d27b6c3b1128e5d6cec96e8282221f
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2023-10-24 21:20:05 -0600

    Tested methods and modified

--- user/views.py
+++ user/views.py
@@ -7,37 +7,37 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['GET'])
-def author_list(request):
+def author_list(request):  #tested
     if request.method=='GET':
-        users=User.objects.all()
+        users=User.objects.filter(type='AUTHOR')
         serializer=UserSerializer(users,many=True)
         return Response(serializer.data)
 @api_view(['POST'])
-def create_author(request):
+def create_author(request): #tested
     if request.method=='POST':
         serializer=UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 @api_view(['GET','PUT','DELETE'])
-def single_author_method(request,pk):
+def single_author_method(request,pk): #tested
     try:
-        user=User.objects.get(pk=pk)
+        user=User.objects.get(pk=pk, type='AUTHOR')
     except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({"message":{f"No author with id {pk} exist"}},status=status.HTTP_404_NOT_FOUND)
     if request.method=='GET':
         serializer=UserSerializer(user)
         return Response(serializer.data,status=status.HTTP_200_OK)
     elif request.method=='PUT':
-        serializer=UserSerializer(user,data=request.data)
+        serializer=UserSerializer(user,data=request.data,partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method=='DELETE':
         user.delete()
-        return Response(status=status.HTTP_200_OK)
+        return Response({"message":{f" author with id {pk} is deleted"}},status=status.HTTP_200_OK)
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def single_comment_method(request,pk):
@@ -53,7 +53,7 @@ def single_comment_method(request,pk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method=='DELETE':
         comment.delete()
         return Response(status=status.HTTP_200_OK)
@@ -64,7 +64,7 @@ def create_comment(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def get_comment_for_post(request,pk):
@@ -74,9 +74,9 @@ def get_comment_for_post(request,pk):
         return Response(serializer.data,status=status.HTTP_200_OK)
 
 @api_view(['GET', 'DELETE', 'PUT'])
-def single_post_method(request,pk):
+def post_method(request,pk,postID):
     try:
-        post=Post.objects.get(pk=pk)
+        post=Post.objects.get(pk=postID)
     except Post.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method=='GET':
@@ -87,22 +87,28 @@ def single_post_method(request,pk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method=='DELETE':
         post.delete()
         return Response(status=status.HTTP_200_OK)
     
 @api_view(['POST'])
-def create_post(request):
+def create_post(request,pk):
     if request.method=='POST':
-        serializer=PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user=User.objects.get(pk=pk)
+            request.data.update({"author":user.id})
+            serializer=PostSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({"message":{f"No author with id {request.data['author']} exist, create post fail"}},status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['DELETE', 'PUT'])
-def single_like_method(request,):
+def single_like_method(request,pk):
     try:
         like=Like.objects.get(pk=pk)
     except Like.DoesNotExist:
@@ -112,7 +118,7 @@ def single_like_method(request,):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method=='DELETE':
         like.delete()
         return Response(status=status.HTTP_200_OK)
@@ -124,7 +130,7 @@ def create_like(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET'])
 def get_like_for_post(request,pk):
@@ -143,11 +149,33 @@ def likes_of_single_author(request,pk):
 @api_view(['GET'])
 def get_followers_of_single_author(request, pk):
     if request.method == 'GET':
-        followers_list1 = Friendship.objects.filter(to_user=pk)
-        followers_list2 = Friendship.objects.filter(from_user=pk, bidirectional=True)
-        followers = list(set(list(followers_list1) + list(followers_list2)))
-        serializer = FriendshipSerializer(followers, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        followers={"type": "followers", "items": []}
+        followers_list1 = Friendship.objects.filter(to_user=pk) # who direct follow the author
+        followers_list2 = Friendship.objects.filter(from_user=pk, bidirectional=True) # who the author follows and the guy follows back
+        for item in followers_list1:
+            followers["items"].append({
+                "id": item.from_user.id,
+                "host": item.from_user.host,
+                "displayName": item.from_user.displayName,
+                "url": item.from_user.url,
+                "host": item.from_user.host,
+                "github": item.from_user.github,
+                "profileImage": item.from_user.profileImage
+            })
+        for item in followers_list2:
+            if item.to_user not in followers["items"]:
+                followers["items"].append({
+                    "id": item.to_user.id,
+                    "host": item.to_user.host,
+                    "displayName": item.to_user.displayName,
+                    "url": item.to_user.url,
+                    "host": item.to_user.host,
+                    "github": item.to_user.github,
+                    "profileImage": item.to_user.profileImage
+                })
+        if len(followers)<1:
+            return Response({"message":{f"No one follow author with id {pk}"}},status=status.HTTP_200_OK)
+        return Response(followers, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def Be_friend(request):
@@ -168,7 +196,7 @@ def Be_friend(request):
                     if serializer.is_valid():
                         serializer.save()
                         return Response(serializer.data, status=status.HTTP_201_CREATED)
-                    return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 @api_view(['GET', 'DELETE', 'PUT'])
 def single_friendship_method(request,pk,fk):
     try:
@@ -183,7 +211,7 @@ def single_friendship_method(request,pk,fk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method=='DELETE':
         friendship.delete()
         return Response(status=status.HTTP_200_OK)
